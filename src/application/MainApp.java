ef5d85930e38ecbf01a9a6264817210dbafcd03f
johan <johans.londonoa1@uqvirtual.edu.co>
2023-04-27 21:12:31 -0500

    Realice cambios en el envio de las pqrs

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -10,15 +10,16 @@ import application.controller.EnvioPQRSController;
 import application.controller.GestionPQRSAsignadasController;
 import application.controller.LoginController;
 import application.controller.ModuleChoiceController;
-import application.model.PQRS;
 import application.model.Implementacion;
 import application.model.Modulo;
+import application.model.PQRS;
 import application.model.Plataforma;
 import application.model.Soporte;
 import application.model.TipoPQRS;
 import application.model.Usuario;
 import application.services.DesarrolladorService;
 import application.services.ImplementacionService;
+import application.services.PQRSService;
 import application.services.SoporteAsignadoService;
 import application.services.SoporteService;
 import application.services.UsuarioService;
@@ -32,112 +33,114 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private BorderPane rootLayout;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Camu Software");
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Camu Software");
 
-        cargarIcono();
-        initRootLayout();
-        showModuleChoice(null, null, 0);
-
-        // Descomentar para prueba funcional rapida de gestion pqrs
-        // showGestionPQRSAsignadas(2, "erochelle1@guardian.co.uk");
-    }
+		cargarIcono();
+		initRootLayout();
+		// showModuleChoice(null, null, 0);
+		showEnvioPQRS("sheddy0@reddit.com");
+		// Descomentar para prueba funcional rapida de gestion pqrs
+		// showGestionPQRSAsignadas(2, "erochelle1@guardian.co.uk");
+	}
 
 	/**
 	 * Returns the main stage.
+	 * 
 	 * @return
 	 */
 	public Stage getPrimaryStage() {
 		return primaryStage;
 	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    /**
-     * Metodo que muestra el icono en la ventana que se este mostrando
-     */
-    private void cargarIcono() {
-    	Image logoImage = new Image(getClass().getResource("view/images/logo.png").toExternalForm());
-        this.primaryStage.getIcons().add(logoImage);
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 	/**
-     * Inicializa el layout raiz.
-     */
-    public void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
+	 * Metodo que muestra el icono en la ventana que se este mostrando
+	 */
+	private void cargarIcono() {
+		Image logoImage = new Image(getClass().getResource("view/images/logo.png").toExternalForm());
+		this.primaryStage.getIcons().add(logoImage);
+	}
 
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Inicializa el layout raiz.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-    /**
-     * Muestra la pantalla inicial para iniciar el módulo de soporte
-     * @param email
-     * @param rol
-     * @param id_desarrollador
-     */
-    public void showModuleChoice(String rol, String email, int id) {
-    	try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/ModuleChoiceView.fxml"));
-            AnchorPane moduleChoice = (AnchorPane) loader.load();
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-            // Set person overview into the center of root layout.
+	/**
+	 * Muestra la pantalla inicial para iniciar el módulo de soporte
+	 * 
+	 * @param email
+	 * @param rol
+	 * @param id_desarrollador
+	 */
+	public void showModuleChoice(String rol, String email, int id) {
+		try {
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ModuleChoiceView.fxml"));
+			AnchorPane moduleChoice = (AnchorPane) loader.load();
 
-            primaryStage.setMinHeight(380);
-            primaryStage.setMinWidth(660);
-            rootLayout.setCenter(moduleChoice);
+			// Set person overview into the center of root layout.
 
-            // Give the controller access to the main app.
-            ModuleChoiceController controller = loader.getController();
-            controller.setMainApp(this, rol, email, id);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			primaryStage.setMinHeight(380);
+			primaryStage.setMinWidth(660);
+			rootLayout.setCenter(moduleChoice);
 
-    /**
-     * Metodo para abrir la ventana de login
-     */
+			// Give the controller access to the main app.
+			ModuleChoiceController controller = loader.getController();
+			controller.setMainApp(this, rol, email, id);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Metodo para abrir la ventana de login
+	 */
 	public void showLogin() {
 		try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/LoginView.fxml"));
-            AnchorPane moduleChoice = (AnchorPane) loader.load();
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/LoginView.fxml"));
+			AnchorPane moduleChoice = (AnchorPane) loader.load();
 
-            // Set person overview into the center of root layout.
+			// Set person overview into the center of root layout.
 
-            primaryStage.setMinHeight(400);
-            primaryStage.setMinWidth(580);
-            primaryStage.setMaxHeight(400);
-            primaryStage.setMaxWidth(580);
-            rootLayout.setCenter(moduleChoice);
+			primaryStage.setMinHeight(400);
+			primaryStage.setMinWidth(580);
+			primaryStage.setMaxHeight(400);
+			primaryStage.setMaxWidth(580);
+			rootLayout.setCenter(moduleChoice);
 
-            // Give the controller access to the main app.
-            LoginController controller = loader.getController();
-            controller.setMainApp(this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Give the controller access to the main app.
+			LoginController controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showRegister() {
@@ -145,110 +148,112 @@ public class MainApp extends Application {
 
 	}
 
-	public void showGestionPQRSAsignadas(int id_desarrollador, String email){
+	public void showGestionPQRSAsignadas(int id_desarrollador, String email) {
 
 		try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/GestionSolicitudes.fxml"));
-            AnchorPane gestionPQRS = (AnchorPane) loader.load();
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/GestionSolicitudes.fxml"));
+			AnchorPane gestionPQRS = (AnchorPane) loader.load();
 
-            // Set person overview into the center of root layout.
-            primaryStage.setMinHeight(680);
-            primaryStage.setMinWidth(1025);
-            rootLayout.setCenter(gestionPQRS);
+			// Set person overview into the center of root layout.
+			primaryStage.setMinHeight(680);
+			primaryStage.setMinWidth(1025);
+			rootLayout.setCenter(gestionPQRS);
 
-            // Give the controller access to the main app.
-            GestionPQRSAsignadasController controller = loader.getController();
-            controller.setMainApp(this, id_desarrollador, email);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Give the controller access to the main app.
+			GestionPQRSAsignadasController controller = loader.getController();
+			controller.setMainApp(this, id_desarrollador, email);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void showDetallePQRS(int idSoporte){
+	public void showDetallePQRS(int idSoporte) {
 
 		try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/DetallePQRSView.fxml"));
-            AnchorPane detallePQRS = (AnchorPane) loader.load();
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/DetallePQRSView.fxml"));
+			AnchorPane detallePQRS = (AnchorPane) loader.load();
 
-    		// Create the dialog Stage.
-    		Stage dialogStage = new Stage();
-    		dialogStage.setTitle("Edit Person");
-    		dialogStage.getIcons().add( new Image(getClass().getResource("view/images/logo.png").toExternalForm()) );
-    		dialogStage.initOwner(primaryStage);
-    		Scene scene = new Scene(detallePQRS);
-    		dialogStage.setScene(scene);
+			// Create the dialog Stage.
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.getIcons().add(new Image(getClass().getResource("view/images/logo.png").toExternalForm()));
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(detallePQRS);
+			dialogStage.setScene(scene);
 
-            // Give the controller access to the main app.
-            DetallePQRSController controller = loader.getController();
-            controller.setMainApp(this, idSoporte);
+			// Give the controller access to the main app.
+			DetallePQRSController controller = loader.getController();
+			controller.setMainApp(this, idSoporte);
 
-            dialogStage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showEnvioPQRS(String email_usuario_ingresado) {
 		try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/EnvioPQRSView.fxml"));
-            AnchorPane gestionPQRS = (AnchorPane) loader.load();
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/EnvioPQRSView.fxml"));
+			AnchorPane gestionPQRS = (AnchorPane) loader.load();
 
-            // Set person overview into the center of root layout.
-            primaryStage.setMinHeight(540);
-            primaryStage.setMinWidth(780);
-            rootLayout.setCenter(gestionPQRS);
+			// Set person overview into the center of root layout.
+			primaryStage.setMinHeight(540);
+			primaryStage.setMinWidth(780);
+			rootLayout.setCenter(gestionPQRS);
 
-            // Give the controller access to the main app.
-            EnvioPQRSController controller = loader.getController();
-            controller.setMainApp(this, email_usuario_ingresado);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Give the controller access to the main app.
+			EnvioPQRSController controller = loader.getController();
+			controller.setMainApp(this, email_usuario_ingresado);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showAsignacionPQRS(String email_asignador_ingresado) {
 		try {
-            // Carga del fxml de eleccion de modulo.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/AsignacionPQRSView.fxml"));
-            AnchorPane gestionPQRS = (AnchorPane) loader.load();
+			// Carga del fxml de eleccion de modulo.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/AsignacionPQRSView.fxml"));
+			AnchorPane gestionPQRS = (AnchorPane) loader.load();
 
-            // Set person overview into the center of root layout.
-            primaryStage.setMinHeight(620);
-            primaryStage.setMinWidth(1000);
-            rootLayout.setCenter(gestionPQRS);
+			// Set person overview into the center of root layout.
+			primaryStage.setMinHeight(620);
+			primaryStage.setMinWidth(1000);
+			rootLayout.setCenter(gestionPQRS);
 
-            // Give the controller access to the main app.
-            AsignacionPQRSController controller = loader.getController();
-            controller.setMainApp(this, email_asignador_ingresado);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Give the controller access to the main app.
+			AsignacionPQRSController controller = loader.getController();
+			controller.setMainApp(this, email_asignador_ingresado);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Metodo para obtener la lista de osportes asignados a un desarrollador segun su id
+	 * Metodo para obtener la lista de osportes asignados a un desarrollador
+	 * segun su id
+	 * 
 	 * @param id_desarrollador
 	 * @return
 	 */
-	public ArrayList<Soporte> listaSoportesAsignados(int id_desarrollador){
+	public ArrayList<Soporte> listaSoportesAsignados(int id_desarrollador) {
 		return SoporteAsignadoService.listaSoportesAsignados(id_desarrollador);
 	}
 
-	public ArrayList<Implementacion> listaImplementacionesSoporte(int id_soporte){
+	public ArrayList<Implementacion> listaImplementacionesSoporte(int id_soporte) {
 		return ImplementacionService.listaImplementacionesSoporte(id_soporte);
 	}
 
-	public void agregarImplementacionSoporteSeleccionado(int idSoporteSeleccionado,
-			String especificacion, LocalDate fecha, int horas, String estado) {
-		ImplementacionService.agregarImplementacionSoporteSeleccionado(idSoporteSeleccionado,
-				especificacion, fecha, horas, estado);
+	public void agregarImplementacionSoporteSeleccionado(int idSoporteSeleccionado, String especificacion,
+			LocalDate fecha, int horas, String estado) {
+		ImplementacionService.agregarImplementacionSoporteSeleccionado(idSoporteSeleccionado, especificacion, fecha,
+				horas, estado);
 	}
 
 	public void actualizarImplementacionSeleccionada(int idImplementacionSeleccionada, int idSoporteSeleccionado,
@@ -297,5 +302,11 @@ public class MainApp extends Application {
 	public void agregarRespuestaSoporte(int idSoporteSeleccionado, String respuesta, String estadoNew) {
 		SoporteService.agregarRespuestaSoporte(idSoporteSeleccionado, respuesta, estadoNew);
 	}
+
+	public void enviarSolicitud(String email, String cedula, String asunto, String detalle, int idTipoSolicitud,
+			int idPlataforma, int idModulo) {
+		PQRSService.crearPQRS(email,cedula,asunto,detalle,idTipoSolicitud,idPlataforma,idModulo);
+	}
+
 
 }
--- src/application/services/UsuarioService.java
+++ src/application/services/UsuarioService.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.apache.commons.codec.digest.DigestUtils;
+
+import application.model.Usuario;
 import application.persistence.MySQLConnector;
 
 public class UsuarioService {
@@ -22,8 +24,8 @@ public class UsuarioService {
 		try {
 			conexion = conector.conectar();
 			stm = conexion.createStatement();
-			rset = stm.executeQuery("SELECT * FROM USUARIO U WHERE U.email = '"+email+
-					"' AND U.password = '"+encriptMD5+"'");
+			rset = stm.executeQuery(
+					"SELECT * FROM USUARIO U WHERE U.email = '" + email + "' AND U.password = '" + encriptMD5 + "'");
 
 			if (rset.next() == true) {
 				valido = true;
@@ -51,6 +53,58 @@ public class UsuarioService {
 		}
 
 		return valido;
+	}
+
+	public static Usuario obtenerUsuarioCedula(String cedula) {
+		Usuario usuarioEncontrado = null;
+		MySQLConnector conector = new MySQLConnector();
+		Connection conexion = null;
+		Statement stm = null;
+		ResultSet rset = null;
+
+		try {
+			conexion = conector.conectar();
+			stm = conexion.createStatement();
+			rset = stm.executeQuery("SELECT * FROM USUARIO U WHERE U.cedula = '" + cedula + "'");
+			
+			//Recorrer todos los datos obtenidos de la sentencia SQL
+			while (rset.next()) {
+				int id = rset.getInt(1);
+				String nombre = rset.getString(2);
+				String apellidos = rset.getString(3);
+				String idCedula = rset.getString(4);
+				String telefono = rset.getString(5);
+				String emailUsuario = rset.getString(6);
+				String direccion = rset.getString(7);
+				String ciudadResidencia = rset.getString(8);
+				String password = rset.getString(9);
+
+				usuarioEncontrado = new Usuario(id, nombre, apellidos, idCedula, telefono, emailUsuario, direccion,
+						ciudadResidencia, password);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (rset != null) {
+					rset.close();
+				}
+				if (stm != null) {
+					stm.close();
+				}
+				if (conexion != null) {
+					conexion.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return usuarioEncontrado;
 	}
 
 }
