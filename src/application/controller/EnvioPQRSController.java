2c544214003fd756be5553b7aa17e69f0f1e1edb
Sebastian Burgos Puentes <sebastian.burgosp@uqvirtual.edu.co>
2023-04-28 19:55:28 -0500

    cambios descartados

--- src/application/controller/EnvioPQRSController.java
+++ src/application/controller/EnvioPQRSController.java
@@ -1,8 +1,6 @@
 package application.controller;
 
 import application.MainApp;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
